364f4ae3611c82d101968dae8e6b704ac75143c2
Epheia <epheia@outlook.com>
2026-05-21 01:52:09 +0800

    :wheelchair: a11y(styles): fix code block backgrounds and divider contrast in dark/high-contrast modes

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,7 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
-      themes: {
-        light: 'github-light',
-        dark: 'github-dark',
-      },
+      wrap: true,
     },
   },
   vite: {
